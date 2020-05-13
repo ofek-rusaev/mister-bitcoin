e4fcdf4d051c0f6e58988526f346c082407f0657
ofek-rusaev <ofek2602@gmail.com>
2020-05-13 13:18:17 +0300

    Updates

---
+++ precache-manifest.d800a5651e23f7632dacdf5a171b8820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c561e032259ac69a0baf271fcc9b96c3",
+    "url": "/mister-bitcoin/index.html"
+  },
+  {
+    "revision": "1d6c6e1f1023032af594",
+    "url": "/mister-bitcoin/static/css/main.9ff851dd.chunk.css"
+  },
+  {
+    "revision": "966e0ebc9b945bac47e7",
+    "url": "/mister-bitcoin/static/js/2.ca193887.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mister-bitcoin/static/js/2.ca193887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6c6e1f1023032af594",
+    "url": "/mister-bitcoin/static/js/main.9d380d5e.chunk.js"
+  },
+  {
+    "revision": "5cc8273a6955055d1a25",
+    "url": "/mister-bitcoin/static/js/runtime-main.14e612e5.js"
+  }
+]);
